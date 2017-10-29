f5e38562b79df3b6d0477018ac9023a143a595d1
Kepha Okari <kephaokari@gmail.com>
2017-10-30 00:03:38 +0300

    jquery method to capture input

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,51 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
     }
   );
+
 $(document).ready(function() {
 	$("button").click(function(e){
-		e.preventDefault();
+		var loopLimit = $("input#text-area").val();// to pick the limit from the input box
+
+		var provitionalTotal = 0;                 //initialize the total
+
+		
+		for (var i =1; i <=loopLimit; i++) {
+			if(i%3=0 $$ i%5!=0){                 // display number divisible only 3
+				$(".verb").append(provitionalTotal);
+			} else if(i%3!=0 $$ i%5=0) {         // display number divisible only 5
+				$(".verb").append(provitionalTotal);
+			} else if(i%15=0 ) {                 // display number 15
+				$(".verb").append(provitionalTotal);
+			} else {							// display numericals
+				$(".verb").append(provitionalTotal);
+			}
+			
+			
+		}
 
 	});
-  
-});
+
+  });
+
+/*
+ $(document).ready(function() {
+        $("#blanks form").submit(function(event) {
+          var person1Input = $("input#person1").val();
+          var person2Input = $("input#person2").val();
+          var animalInput= $("input#animal").val();
+          var exclamationInput = $("input#exclamation").val();
+          var verbInput = $("input#verb").val();
+          var nounInput = $("input#noun").val();
+          $(".person1").append(person1Input);
+          $(".person2").append(person2Input);
+          $(".animal").append(animalInput);
+          $(".exclamation").append(exclamationInput);
+          $(".verb").append(verbInput);
+          $(".noun").append(nounInput);
+
+          $("#story").show();
+
+          event.preventDefault();
+        });
+      });
+*/
